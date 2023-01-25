0cdd800baa682c3a7edcf1f14e1518318fe31bc9
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-01-25 14:55:41 +0300

    first version

--- lib/main.dart
+++ lib/main.dart
@@ -48,68 +48,140 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green[400],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("assets/images/mithat.jpg"),
+              ),
+              const Text(
+                "Bilgi Kartları",
+                style: TextStyle(
+                    fontFamily: "Pacifico",
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Aradığınız TYT - AYT Bilgileri Burada",
+                style: TextStyle(
+                  fontFamily: "Source Sans Pro",
+                  fontSize: 15.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green[100],
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                    horizontal: 25.0, vertical: 10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.book,
+                      color: Colors.green,
+                    ),
+                    title: const Text(
+                      "TYT Kartlar",
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                    trailing: GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () {
+                        /* Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => TytPage())); */
+                      },
+                      child: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                    horizontal: 25.0, vertical: 10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.calculate,
+                      color: Colors.green,
+                    ),
+                    title: const Text(
+                      "AYT Kartlar",
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                    trailing: GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () {
+                        /* Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => AytPage())); */
+                      },
+                      child: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.school,
+                      color: Colors.green,
+                    ),
+                    title: const Text(
+                      "TYT - AYT Kartlar",
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                    trailing: GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () {
+                        /* Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => TytAytPage())); */
+                      },
+                      child: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+        // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
