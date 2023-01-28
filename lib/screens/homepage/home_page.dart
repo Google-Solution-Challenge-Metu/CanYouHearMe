e9764072358c6d4e9f64f6dbd81dcf46843ab07e
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-01-28 12:38:22 +0300

    first version

---
+++ lib/screens/homepage/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
