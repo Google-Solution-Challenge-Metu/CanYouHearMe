0ba605b97d810dc76a3c9d4b5cc8d15cb75de7f9
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-01-31 16:57:36 +0300

    first version

---
+++ lib/screens/explore/explore.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+
+class ExploreScreen extends StatelessWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("Explore"),
+      ),
+    );
+  }
+}
