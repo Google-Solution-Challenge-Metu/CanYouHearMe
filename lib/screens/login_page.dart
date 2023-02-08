c8eea9f4af7cf28c182969f396ad48532b38b589
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-02-08 19:54:10 +0300

    login page bug fix

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -66,149 +66,147 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFF4E6C50),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage("assets/images/mithat.jpg"),
-                ),
-                const Text(
-                  "Can You Hear Me?",
-                  style: TextStyle(
-                      fontFamily: "Pacifico",
-                      fontSize: 30.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "to save someone from the earthquake",
-                  style: TextStyle(
-                    fontFamily: "Source Sans Pro",
-                    fontSize: 15.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green[100],
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                  width: 200.0,
-                  child: Divider(
+    return Scaffold(
+      backgroundColor: Color(0xFF4E6C50),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("assets/images/mithat.jpg"),
+              ),
+              const Text(
+                "Can You Hear Me?",
+                style: TextStyle(
+                    fontFamily: "Pacifico",
+                    fontSize: 30.0,
                     color: Colors.white,
-                    thickness: 3.0,
-                  ),
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "to save someone from the earthquake",
+                style: TextStyle(
+                  fontFamily: "Source Sans Pro",
+                  fontSize: 15.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green[100],
                 ),
-                buildTextField(emailController, "E-mail", false),
-                const SizedBox(
-                  height: 20.0,
+              ),
+              const SizedBox(
+                height: 30.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 3.0,
                 ),
-                buildTextField(passwordController, "Password", true),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Şifrenizi mi unuttunuz?",
-                        style: TextStyle(color: Colors.grey.shade200),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                buildButton(signUserIn),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 1,
-                          color: Colors.grey.shade200,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          "Veya şununla devam edin",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 1,
-                          color: Colors.grey.shade200,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                        ),
-                        color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      height: 50.0,
-                      child: Image.asset("assets/images/google.png"),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              buildTextField(emailController, "E-mail", false),
+              const SizedBox(
+                height: 20.0,
+              ),
+              buildTextField(passwordController, "Password", true),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Text(
-                      "Kayıtlı değil misin?",
-                      style: TextStyle(
-                        color: Colors.grey[300],
+                      "Şifrenizi mi unuttunuz?",
+                      style: TextStyle(color: Colors.grey.shade200),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              buildButton(signUserIn),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 1,
+                        color: Colors.grey.shade200,
                       ),
                     ),
-                    GestureDetector(
-                      onTap: registerUser,
-                      child: const Text(
-                        " Kaydol",
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        "Veya şununla devam edin",
                         style: TextStyle(
                           color: Colors.white,
-                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 1,
+                        color: Colors.grey.shade200,
+                      ),
+                    ),
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    height: 50.0,
+                    child: Image.asset("assets/images/google.png"),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Kayıtlı değil misin?",
+                    style: TextStyle(
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: registerUser,
+                    child: const Text(
+                      " Kaydol",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
-        // This trailing comma makes auto-formatting nicer for build methods.
       ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
