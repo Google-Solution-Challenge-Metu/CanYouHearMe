e855649e6c3bbf47ff2e15c4ee9ba232b72ab145
AlperenOvak <alperen.ovak@metu.edu.tr>
2023-02-11 11:48:43 +0300

    little changes

--- lib/screens/help/volunteer.dart
+++ lib/screens/help/volunteer.dart
@@ -1,4 +1,6 @@
+import 'package:dietapp/screens/help/glassbox.dart';
 import "package:flutter/material.dart";
+
 
 class VolunteerScreen extends StatefulWidget {
   const VolunteerScreen({super.key});
@@ -9,11 +11,55 @@ class VolunteerScreen extends StatefulWidget {
 class _VolunteerScreenState extends State<VolunteerScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text("Volunteer"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF4E6C50),
+        title: Text("Let's Help!"),
       ),
-    );
+      backgroundColor: Colors.grey[300],
+      body: 
+      Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            // Import image in pubspec.yaml first
+            image: AssetImage('assets/images/Colorcard-hex_820000.png'),
+            // BoxFit.cover scales up the image until it completely covers the screen
+            fit: BoxFit.cover,
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FrostedGlassBox(
+                  theWidth: 350.0, 
+                  theHeight: 200.0, 
+                  theChild: Text("data")),
+              ),
+              
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FrostedGlassBox(
+                  theWidth: 350.0, 
+                  theHeight: 200.0, 
+                  theChild: Text("data")),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FrostedGlassBox(
+                  theWidth: 350.0, 
+                  theHeight: 200.0, 
+                  theChild: Text("data")),
+              ),
+            
+          ],
+        ),
+          
+
+        ),
+      
+      );
+    
   }
 }
