9763f3342d0e728633401454ea6e9df7b740cc6d
AlperenOvak <alperen.ovak@metu.edu.tr>
2023-02-20 15:13:16 +0300

    Revert "Ready to start wearOS"

--- android/app/src/main/kotlin/com/example/dietapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dietapp/MainActivity.kt
@@ -2,16 +2,5 @@ package com.example.dietapp
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity(), AmbientMode.AmbientCallbackProvider {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-
-    // Wire up the activity for ambient callbacks
-    AmbientMode.attachAmbientSupport(this)
-  }
-
-  override fun getAmbientCallback(): AmbientMode.AmbientCallback {
-    return FlutterAmbientCallback(getChannel(flutterView))
-  }
+class MainActivity: FlutterActivity() {
 }
